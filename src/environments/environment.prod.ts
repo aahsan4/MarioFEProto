8867f6a2b842b4c628db4e11c296a83768ffad8b
Aninda Ahsan <118940164+aahsan4@users.noreply.github.com>
2025-02-09 09:32:22 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://backend-app.us-east-1.elasticbeanstalk.com' // Replace with your backend URL
+  apiUrl: 'http://mariobackend-env.eba-eefm6dxx.us-east-2.elasticbeanstalk.com/' 
 };
 
